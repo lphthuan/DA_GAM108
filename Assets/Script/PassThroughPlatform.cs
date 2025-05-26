e179b6485b22e63f6e2aa8bdfd410204a74167f3
Le Phan Hoa Thuan <lephanhoathuan2006@gmail.com>
2025-05-26 18:18:15 +0700

    Update Platform

--- Assets/Script/PassThroughPlatform.cs
+++ Assets/Script/PassThroughPlatform.cs
@@ -1,18 +1,47 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PassThroughPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private Collider2D _collider;
+	private bool _playerOnPlatform;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Start()
+	{
+		_collider = GetComponent<Collider2D>();
+	}
+
+	void Update()
+	{
+		if (_playerOnPlatform && Input.GetAxisRaw("Vertical") < 0)
+		{
+			_collider.enabled = false;
+			StartCoroutine(EnableCollider());
+		}
+	}
+
+	private IEnumerator EnableCollider()
+	{
+		yield return new WaitForSeconds(0.5f);
+		_collider.enabled = true;
+	}
+
+	private void SetPlayerOnPlatform(Collider2D other, bool value)
+	{
+		var player = other.gameObject.GetComponent<PlayerMovement>();
+		if (player != null)
+		{
+			_playerOnPlatform = value;
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		SetPlayerOnPlatform(other.collider, true);
+	}
+
+	private void OnCollisionExit2D(Collision2D other)
+	{
+		SetPlayerOnPlatform(other.collider, false);
+	}
 }
