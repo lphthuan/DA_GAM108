963b8422ff7818479075e1aec0f7b5757f3a5240
Le Phan Hoa Thuan <lephanhoathuan2006@gmail.com>
2025-05-30 17:36:26 +0700

    Trap, exit

---
+++ Assets/Script/Trap/TrapController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapController : MonoBehaviour
+{
+	[SerializeField] private Transform spawnPoint; // Điểm hồi sinh
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player"))
+		{
+			if (spawnPoint != null)
+			{
+				collision.transform.position = spawnPoint.position;
+			}
+			else
+			{
+				Debug.LogWarning("Chưa gán spawn point cho gai!");
+			}
+		}
+	}
+}
